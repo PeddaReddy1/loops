4ac6e2e07dfe75c28c2bad38792a2c13924d816d
PeddaReddy1 <91711438+PeddaReddy1@users.noreply.github.com>
2021-10-03 08:16:40 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,20 @@
+public class Switch{
+public static void main(String args[]){
+int a=5;
+int b=10;
+String reddy="march";
+switch(reddy){
+case "january":System.out.println("Addition"+(a+b));
+break;
+case "march":System.out.println("Substration"+(a-b));
+break;
+
+case "april":System.out.println("Multiplication"+(a*b));
+break;
+
+default:System.out.println("Invalid");
+
+	}
+}
+}
+0.
